0a336c757fbcc7a95fa84609db9d67cda3c3824e
SKMPranto <prantokumar3463@gmail.com>
2025-09-28 09:44:16 +0600

    manage tasks implemented successfully

--- src/Pages/Dashboard/AdminDashboard/ManageTasks/AllTasks.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageTasks/AllTasks.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import Swal from "sweetalert2";
-import { NavLink } from "react-router";
-import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 
-const MyAddedTasks = ({ tasks, setTasks }) => {
+const AllTasks = ({ tasks, setTasks }) => {
   const axiosSecure = useAxiosSecure();
-
   //  Remove task from UI
   const handleDeleteFromList = (id) => {
     setTasks((prev) => prev.filter((task) => task._id !== id));
@@ -67,35 +64,17 @@ const MyAddedTasks = ({ tasks, setTasks }) => {
         }
       });
   };
-
-  //  Sort tasks by completion_date descending
-  const sortedTasks = [...tasks].sort(
-    (a, b) => new Date(b.completion_date) - new Date(a.completion_date)
-  );
-
-  if (!tasks || tasks.length === 0) {
+  if (tasks.length === 0) {
     return (
-      <div className="flex flex-col items-center justify-center h-[60vh] text-center space-y-4">
-        <h2 className="text-lg md:text-4xl lg:text-5xl font-bold text-red-300">
-          You haven’t added any Tasks yet.
-        </h2>
-        <p className="text-gray-400 md:text-2xl">
-          Start contributing by adding your first Task !
-        </p>
-        <NavLink
-          to="/dashboard/addNewTasks"
-          className="btn btn-outline btn-info md:text-2xl font-bold rounded"
-        >
-          Add task
-        </NavLink>
-      </div>
+      <h2 className="text-3xl font-semibold text-center mt-10">
+        No Tasks Available
+      </h2>
     );
   }
-
   return (
     <div>
       <h1 className="text-2xl md:text-3xl 2xl:text-4xl text-red-400 font-bold text-center mt-10">
-        My Added Tasks
+        All Tasks
       </h1>
 
       {/*  Table Format */}
@@ -103,6 +82,7 @@ const MyAddedTasks = ({ tasks, setTasks }) => {
         <table className="table table-zebra w-full">
           <thead>
             <tr>
+                <th></th>
               <th>Title</th>
               <th>Detail</th>
               <th>Required Workers</th>
@@ -113,8 +93,9 @@ const MyAddedTasks = ({ tasks, setTasks }) => {
             </tr>
           </thead>
           <tbody>
-            {sortedTasks.map((task) => (
+            {tasks.map((task ,index) => (
               <tr key={task._id}>
+                <td>{index + 1}</td>
                 <td>{task.task_title}</td>
                 <td>{task.task_detail}</td>
                 <td>{task.required_workers}</td>
@@ -122,12 +103,6 @@ const MyAddedTasks = ({ tasks, setTasks }) => {
                 <td>{new Date(task.completion_date).toLocaleDateString()}</td>
                 <td>{task.submission_info}</td>
                 <td className="flex gap-2">
-                  <NavLink
-                    to={`/dashboard/updateTask/${task._id}`}
-                    className="btn btn-sm btn-warning rounded font-bold flex items-center gap-1"
-                  >
-                    <FaEdit /> Update
-                  </NavLink>
                   <button
                     onClick={() => handleDelete(task._id)}
                     className="btn btn-sm btn-error rounded font-bold flex items-center gap-1"
@@ -143,5 +118,4 @@ const MyAddedTasks = ({ tasks, setTasks }) => {
     </div>
   );
 };
-
-export default MyAddedTasks;
+export default AllTasks;
--- src/Pages/Dashboard/AdminDashboard/ManageTasks/ManageTasks.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageTasks/ManageTasks.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import Title from '../../../../Sheared/Title/Title';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
+import Loader from '../../../../Sheared/LoaderEffect/Loader';
+import AllTasks from './AllTasks';
 
 const ManageTasks = () => {
+    Title("Dashboard | Manage Tasks");
+    const axiosSecure = useAxiosSecure();
+    const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        setLoading(true);
+        axiosSecure.get("tasks").then((res)=>{
+            setTasks(res.data);
+        }).catch((err)=>console.error(err))
+        .finally(()=>setLoading(false));
+    }, [axiosSecure])
     return (
         <div>
-            <h1>Manage Tasks</h1>
+            <Suspense fallback={<Loader></Loader>}>
+            <AllTasks tasks={tasks} setTasks={setTasks}></AllTasks>
+            </Suspense>
         </div>
     );
 };
